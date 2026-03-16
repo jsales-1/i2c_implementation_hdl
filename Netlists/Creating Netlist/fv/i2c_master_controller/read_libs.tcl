57b55282249e37532f96b6b6995bc6e022833cdb
jsales-1 <davjos33@gmail.com>
2026-03-16 15:53:38 -0300

    Removing old netlists

--- Netlists/Creating Netlist/fv/i2c_master_controller/read_libs.tcl
+++
@@ -1,5 +0,0 @@
-add_search_path ../LIB/ -library -both
-read_library -liberty -both \
-    /prj/ci/workarea/aluno6/Verilog/i2c_implementation_hdl-main/Netlists/Creating \
-    Netlist/../LIB/slow_vdd1v0_basicCells.lib
-
